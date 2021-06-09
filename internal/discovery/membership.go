bb3cb17b08797b3cae004f3747c375bd3a3166c0
Ian Clark <idclark13@gmail.com>
2021-06-09 14:46:09 -0700

    initial service membership discovery

---
+++ internal/discovery/membership.go
@@ -0,0 +1,62 @@
+package discovery
+
+import (
+	"net"
+
+	"github.com/hashicorp/serf/serf"
+	"go.uber.org/zap"
+)
+
+type Membership struct {
+	Config
+	handler Handler
+	serf    *serf.Serf
+	events  chan serf.Event
+	logger  *zap.Logger
+}
+
+type Config struct {
+	NodeName       string
+	BindAddr       string
+	Tags           map[string]string
+	StartJoinAddrs []string
+}
+
+func New(handler Handler, config Config) (*Membership, error) {
+	c := &Membership{
+		Config:  config,
+		handler: handler,
+		logger:  zap.L().Named("membership"),
+	}
+	if err := c.setupSerf(); err != nil {
+		return nil, err
+	}
+	return c, nil
+}
+
+func (m *Membership) setupSerf() (err error) {
+	addr, err := net.ResolveTCPAddr("tcp", m.BindAddr)
+	if err != nil {
+		return err
+	}
+	config := serf.DefaultConfig()
+	config.Init()
+	config.MemberlistConfig.BindAddr = addr.IP.String()
+	config.MemberlistConfig.BindAddr = addr.Port
+	m.events = make(chan serf.Event)
+	config.EventCh = m.events
+	config.Tags = m.Tags
+	config.NodeName = m.Config.NodeName
+	m.serf, err = serf.Create(config)
+	if err != nil {
+		return err
+	}
+	go m.eventHandler()
+	if m.StartJoinAddrs != nil {
+		_, err = m.serf.Join(m.StartJoinAddrs, true)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
