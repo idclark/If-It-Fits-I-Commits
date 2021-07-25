802184997956b91e75b490838e75ac0e82d393d6
Ian Clark <idclark13@gmail.com>
2021-07-25 15:39:06 -0400

    fixing import

--- internal/loadbalance/resolver.go
+++ internal/loadbalance/resolver.go
@@ -11,7 +11,7 @@ import (
 	"google.golang.org/grpc/resolver"
 	"google.golang.org/grpc/serviceconfig"
 
-	api "github.com/idclark/ifitfitsisits/api/v1"
+	api "github.com/idclark/ifitfitsicommits/api/v1"
 )
 
 type Resolver struct {
